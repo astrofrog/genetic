ba6259c154c5a3dcae4d10d339463dde4ed92a16
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-09-03 22:44:29 +0200

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Genetic Algorithm',
-      version='0.1.3',
+      version='0.1.2',
       description='Simple parallel genetic algorithm',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
